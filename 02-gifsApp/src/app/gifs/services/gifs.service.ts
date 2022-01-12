4db123e7cf4f4a9011dbb6645484e96fd7adcf7d
Francisco Herrera <fran.herrera124@gmail.com>
2022-01-12 00:55:40 -0300

    avances del curso de angular

---
+++ 02-gifsApp/src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Gif, SearchGifsResponse } from '../interface/gifs.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GifsService {
+  private apiKey: string = 'oFGBVuSzzbu4WKKYLZFKR1SCdafcx0Qv';
+
+  private _historial: string[] = [];
+
+  public resultados: Gif[] = [];
+
+  get historial() {
+    return [...this._historial];
+  }
+
+  constructor(private http: HttpClient) {}
+
+  buscarGifs(query: string) {
+    query = query.trim().toLocaleLowerCase();
+    if (!this._historial.includes(query)) {
+      this._historial.unshift(query);
+      this._historial = this._historial.splice(0, 10);
+    }
+
+    console.log(this._historial);
+
+    this.http
+      .get<SearchGifsResponse>(
+        `https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${query}&limit=10`
+      )
+      .subscribe((resp) => {
+        console.log(resp);
+        this.resultados = resp.data;
+      });
+  }
+}
