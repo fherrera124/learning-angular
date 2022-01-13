b6665bd5dbf71dad0f9c70f5f559743dde6f6db5
Francisco Herrera <fran.herrera124@gmail.com>
2022-01-13 01:56:57 -0300

    poco avance

--- 02-gifsApp/src/app/gifs/services/gifs.service.ts
+++ 02-gifsApp/src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Gif, SearchGifsResponse } from '../interface/gifs.interface';
 
@@ -8,6 +8,8 @@ import { Gif, SearchGifsResponse } from '../interface/gifs.interface';
 export class GifsService {
   private apiKey: string = 'oFGBVuSzzbu4WKKYLZFKR1SCdafcx0Qv';
 
+  private servicioUrl: string = 'https://api.giphy.com/v1/gifs';
+
   private _historial: string[] = [];
 
   public resultados: Gif[] = [];
@@ -16,24 +18,37 @@ export class GifsService {
     return [...this._historial];
   }
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+    this.resultados = JSON.parse(localStorage.getItem('resultados')!) || [];
 
-  buscarGifs(query: string) {
+    // if( localStorage.getItem('historial') ){
+    //   this._historial = JSON.parse( localStorage.getItem('historial')! );
+    // }
+  }
+
+  buscarGifs(query: string = '') {
     query = query.trim().toLocaleLowerCase();
     if (!this._historial.includes(query)) {
       this._historial.unshift(query);
       this._historial = this._historial.splice(0, 10);
+
+      localStorage.setItem('historial', JSON.stringify(this._historial));
     }
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('q', query)
+      .set('limit', '10');
 
     console.log(this._historial);
 
     this.http
-      .get<SearchGifsResponse>(
-        `https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${query}&limit=10`
-      )
+      .get<SearchGifsResponse>(`${this.servicioUrl}/search`, { params: params })
       .subscribe((resp) => {
         console.log(resp);
         this.resultados = resp.data;
+        localStorage.setItem('resultados', JSON.stringify(this.resultados));
       });
   }
 }
